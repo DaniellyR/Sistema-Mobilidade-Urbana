023ed2d98646946cc516414a0fbda5a53587b65b
Danielly Reis <reisdanny209@gmail.com>
2025-11-28 20:54:08 -0300

    unificação master → main + implementação Usuarios + oranização das pastas

--- src/entidades/Usuario.java
+++ src/entidades/Usuario.java
@@ -1,5 +1,24 @@
 package entidades;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Usuario {
-    
+    private String nome;
+    private String CPF;
+    private String email;
+    private String senha;
+    private String telefone;
+    private double mediaNota;
+    private List<Integer> avaliacoes;
+
+    public Usuario (String nome, String CPF, String email, String senha, String telefone){
+        this.nome = nome;
+        this.telefone = telefone;
+        this.CPF = CPF;
+        this.email = email;
+        this.senha = senha;
+        this.mediaNota = 5.0;
+        this.avaliacoes = new ArrayList<>();
+    }
 }
