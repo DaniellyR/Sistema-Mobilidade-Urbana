e321969ea1c3c0d4903ab32fbc8ab47da3f5e034
Danielly Reis <reisdanny209@gmail.com>
2025-11-29 01:39:30 -0300

    implementação do Passageiro e Corrida(incompleta)

--- src/entidades/Passageiro.java
+++ src/entidades/Passageiro.java
@@ -1,5 +1,50 @@
 package entidades;
 
-public class Passageiro {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Passageiro extends Usuario {
+    private List<MetodoPagamento> meusPagamentos;
+    private Corrida corridaAtual;
+    private boolean possuiPendencia;
     
+    public Passageiro(String nome, String CPF, String email, String senha, String telefone) {
+        super(nome, CPF, email, senha, telefone);
+        this.meusPagamentos = new ArrayList<>();
+        this.corridaAtual = null;
+        this.possuiPendencia = false; //ele inicia sem
+    }
+
+    public List<MetodoPagamento> getMeusPagamentos(){
+        return meusPagamentos;
+    }
+    public void setMeuPagamentos(List<MetodoPagamento> meusPagamentos){
+        this.meusPagamentos = meusPagamentos;
+    }
+
+    public Corrida getCorridaAtual(){
+        return corridaAtual;
+    }
+    public void setCorridaAtual(Corrida c){ 
+        this.corridaAtual = c;
+    }
+
+    public boolean getPossuiPendencia(){
+        return possuiPendencia; 
+    }
+    public void setPossuiPendencia( boolean status){ //pq status
+        this.possuiPendencia = status; 
+
+    }
+
+    public void adicionarMetodoPagamento(MetodoPagamento mp){
+        this.meusPagamentos.add(mp);
+    }
+
+    public void solicitarCorrida(String origem, String destino){
+        this.corridaAtual = new Corrida(this, origem, destino);
+
+    }
+    
+
 }
